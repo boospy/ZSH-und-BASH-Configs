e89d7db4e9b25ba902000ee01065d7b91ea13e18
Mario Loderer <mario@bodyguide.at>
2021-05-16 23:40:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+ZSH, Bash und Nano Config
+=========================
+
+Hier findest du vorgefertigte Configs für Nano, ZSH und Bash. Die Shellconfigs basieren auf dem Projekt powerlevel10k.
+[GIT Projekt Powerlevel10k](https://github.com/romkatv/powerlevel10k) und der Font von hier: [GIT Projekt Nerd-Fonts](https://github.com/ryanoasis/nerd-fonts) Für V2 wurden Dinge wie Powerlevel10k weggelassen. 
+Du hast die Möglichkeit mit dem automatischem Installer Nano und ZSHconfigs zu installieren. Hierfür einfach den [InstallerV1](https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/blob/master/zsh-easyinstaller.sh) oder am besten gleich [InstallerV2](https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/blob/master/zsh-easyinstallerV2.sh) herunterladen, ausführbar machen und ausführen. 
+
+INSTALLATION
+------------
+
+Folgende Befehle mit SUDO oder in einer Rootshell ausführen.
+
+**V1**
+~~~
+wget https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/zsh-easyinstaller.sh
+chmod +x zsh-easyinstaller.sh
+./zsh-easyinstaller.sh
+~~~
+
+**V2**
+~~~
+wget https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/zsh-easyinstallerV2.sh
+chmod +x zsh-easyinstallerV2.sh
+./zsh-easyinstallerV2.sh
+~~~
+
+
+Die Bashrc hat folgende Modfikationen: 
+
+  * Die Limitierung der History auf eine bestimmte Anzahl wurde entfernt.
+  * Bash schreibt die History nicht erst beim Logout, sondern im Livemodus.
---
+++ zsh-easyinstaller.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# (c) Mario Loderer
+# linux@osit.cc
+# https://deepdoc.at
+#
+
+echo -------------------------------------
+echo "Das Script installiert die ZSHshell als Default Rootshell und setzt Syntax Highlighting für Nano."
+echo "Weiters wird eine Benutzervorlage für ZSH in /etc/skel abgelegt."
+echo "Die Schriftart wird global im System installiert"
+echo "Installer für Ubuntu 18/20.04" 
+echo -------------------------------------
+echo
+echo "Zum vortsetzen ENTER drücken, oder STRG+C zum Abbrechen"
+echo
+read x
+
+apt update
+apt install zsh git -y
+mkdir /usr/share/fonts/truetype/nerdfont && cd /usr/share/fonts/truetype/nerdfont
+wget -O /tmp/Sauce_Code_Pro_Nerd_Font_Complete_Mono.ttf https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/Sauce_Code_Pro_Nerd_Font_Complete_Mono.ttf
+mv /tmp/Sauce_Code_Pro_Nerd_Font_Complete_Mono.ttf /usr/share/fonts/truetype/nerdfont/Sauce_Code_Pro_Nerd_Font_Complete_Mono.ttf
+fc-cache -fv
+wget -O /root/.zshrc https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/zshrc-root
+wget -O /etc/skel/.zshrc https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/zshrc-user
+wget -O /root/.p10k.zsh https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/p10k.zsh-root
+wget -O /etc/skel/.p10k.zsh https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/p10k.zsh-user
+usermod -s /bin/zsh root
+wget -O /tmp/nano.tar https://darkdevil.osit.cc/gitlab/public-projects/zsh-und-bash-configs/raw/master/nano_syntax_highlighting.tar
+tar -xf /tmp/nano.tar -C /root
+tar -xf /tmp/nano.tar -C /etc/skel
+rm /tmp/nano.tar -f
+
+echo
+echo "Vergiss nicht in deinem Terminalprogramm die Schriftart auf Sauce_Code_Pro_Nerd_Font_Complete_Mono.ttf um zu stellen."
+echo
+echo
+
+exit
